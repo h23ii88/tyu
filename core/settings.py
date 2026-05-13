5e9719b7065da8b319de3ffd5689824789553554
Harshit Shukla <harsh280105@gmail.com>
2026-05-13 22:42:44 +0530

    Fix static files settings for production

--- core/settings.py
+++ core/settings.py
@@ -43,13 +43,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # ... rest of your middleware
 ]
+
 
 ROOT_URLCONF = 'core.urls'
 
@@ -116,7 +113,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
 import os
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'calculator', 'static')]
+
+STATIC_URL = '/static/'
+
+# Optional: if you keep extra static files at project root
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# This is where collectstatic will gather everything for production
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
